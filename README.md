e4ad26e7d9898342203ae82e0757128732efd873
cy <chanwit.yimneam@hotmail.com>
2024-12-25 18:30:06 +0700

    Add SQLite database integration and user schema setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 https://v2.tauri.app/start/create-project/#manual-setup-tauri-cli
+https://orm.drizzle.team/docs/get-started/bun-sqlite-new
---
+++ src/db/seed.ts
@@ -0,0 +1,40 @@
+import { drizzle } from 'drizzle-orm/bun-sqlite';
+import { eq } from 'drizzle-orm';
+import { usersTable } from './schema/user';
+  
+const db = drizzle(process.env.DB_FILE_NAME!);
+
+async function main() {
+  const user: typeof usersTable.$inferInsert = {
+    name: 'John',
+    age: 30,
+    email: 'john@example.com',
+  };
+
+  await db.insert(usersTable).values(user);
+  console.log('New user created!')
+
+  const users = await db.select().from(usersTable);
+  console.log('Getting all users from the database: ', users)
+  /*
+  const users: {
+    id: number;
+    name: string;
+    age: number;
+    email: string;
+  }[]
+  */
+
+  await db
+    .update(usersTable)
+    .set({
+      age: 31,
+    })
+    .where(eq(usersTable.email, user.email));
+  console.log('User info updated!')
+
+//   await db.delete(usersTable).where(eq(usersTable.email, user.email));
+//   console.log('User deleted!')
+}
+
+main();
